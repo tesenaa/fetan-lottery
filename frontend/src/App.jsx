8c71fe65e71e2c5633d51a8793c707513340f0b1
tesenaa <tesena7@gmail.com>
2026-08-07 10:42:38 -0700

    feat: added top nav buttons, spin arrow pointer animation,and winner display box

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-[8/7import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
   const [selectedNumbers, setSelectedNumbers] = useState([]);
