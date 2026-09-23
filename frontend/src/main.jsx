dacbe36582da9243cdb51b00d1a8c7a681e418c4
Tesfaye-ad <tesena27@gmail.com>
2026-09-23 04:41:25 -0700

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // 👈 ካለዎት የግሎባል ስታይል ፋይል ያክሉ
 
-// 🌐 Telegram WebApp ማስጀመሪያ
-if (window.Telegram?.WebApp) {
-  window.Telegram.WebApp.ready();
-  window.Telegram.WebApp.expand();
-  window.Telegram.WebApp.setHeaderColor('#0c0c1e');
-  window.Telegram.WebApp.setBackgroundColor('#0c0c1e');
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+)
